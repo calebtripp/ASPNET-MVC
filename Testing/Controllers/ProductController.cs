1d3389db1f83cd1922cd695ea053efc6c31ba23c
calebtripp <calebtripp@outlook.com>
2022-10-19 17:25:58 -0400

    Exercise Complete

---
+++ Testing/Controllers/ProductController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Testing.Controllers
+{
+    public class ProductController : Controller
+    {
+        private readonly IProductRepository repo;
+
+        public ProductController(IProductRepository repo)
+        {
+            this.repo = repo;
+        }
+
+        //get controller
+        public IActionResult Index()
+        {
+            var products = repo.GetAllProducts();
+            return View(products);
+        }
+
+        //public IActionResult Index()
+        //{
+        //    return View();
+        //}
+    }
+}
