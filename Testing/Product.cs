6f43afd4e079d16c02634ffa744b3b7360702476
calebtripp <calebtripp@outlook.com>
2022-10-20 10:40:18 -0400

    Create Product Feature Complete

--- Testing/Product.cs
+++ Testing/Product.cs
@@ -1,4 +1,7 @@
 ﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+
 namespace Testing.Models;
 
 public class Product
@@ -13,4 +16,5 @@ public class Product
     public int CategoryID { get; set; }
     public int OnSale { get; set; }
     public int StockLevel { get; set; }
+    public IEnumerable<Category> Categories { get; set; }
 }
